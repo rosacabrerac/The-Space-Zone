e3b9dfd548a46bc371488a5800ff68840d54de77
Rosa Cabrera <88330990+rosacabrerac@users.noreply.github.com>
2021-10-01 15:05:06 -0300

    01/10
    Avances al 30/09

--- js/foto-del-dia.js
+++ js/foto-del-dia.js
@@ -1,34 +1,22 @@
-/* async function apiRequest() {
+//descomentar 2 y 16 para transformar en carga automatica al cargar html
+// document.addEventListener("DOMContentLoaded", async function (e) {
+
+async function apiRequest() {
   let API_KEY = "RhghDJCmtGLYFa07eFBI2yXU13m7XC2W5BhgmpR3";
   let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
-  console.log(response);
-  let data = await response.json();
-  console.log(data);
-  fotoDia(data);
-}
+  
+  if (response.status === 200) {
+    console.log(response);
+    let data = await response.json();
+    console.log(data);
+
+    fotoDia(data);
+  }
+};
+// });
 
 function fotoDia(data) {
-  document.getElementById(
-    "fotoDia"
-  ).innerHTML = `<img class="centerImage" style="max-width:100%;height:auto;" src="${data.url}">`;
-}
-
-/* Comienzo Botón Mostrar Foto 
-
-function usarApiReq(data) {
-  document.getElementById("APODimagen").innerHTML = `
-    <p id="APODtituloDeImagen">La imagen del dia de hoy es: </p>
-    <img class="centerImage" style="max-width:100%;height:auto;" src="${data.url}">`;
-  document.getElementById("APODexplicacion").innerHTML += data.explanation;
-}
-
-function imagenPedida(data) {
-  document.getElementById("APODimagen").innerHTML += `<img src="${data.url}">`;
-}
-
-/* Fin Botón Mostrar Foto 
-
-window.addEventListener("DOMContentLoaded", (event) => {
-  apiRequest();
-});
-*/
+  document.getElementById("fotoDia").innerHTML = `
+  <p id="APODtituloDeImagen">La imagen del dia de hoy es: </p>
+  <img class="centerImage" id="imagenAPODhd" style="max-width:100%;height:auto;" src="${data.hdurl}">`;
+};
